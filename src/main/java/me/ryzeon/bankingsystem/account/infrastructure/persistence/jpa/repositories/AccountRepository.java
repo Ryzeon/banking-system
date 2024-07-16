49fbba123752fdc672a1857d1492a805d92467b4
Alex Avila <alex.ramon2004@gmail.com>
2024-07-15 20:15:58 -0500

    feat(account): account outbound service.

---
+++ src/main/java/me/ryzeon/bankingsystem/account/infrastructure/persistence/jpa/repositories/AccountRepository.java
@@ -0,0 +1,13 @@
+package me.ryzeon.bankingsystem.account.infrastructure.persistence.jpa.repositories;
+
+import me.ryzeon.bankingsystem.account.domain.model.aggregates.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, String> {
+
+    Optional<Account> findByAccountNumber(String accountNumber);
+}
