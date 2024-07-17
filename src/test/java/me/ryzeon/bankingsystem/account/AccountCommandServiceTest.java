1c8bf4bfbd07edd5097a33215aa4c3ffcec95986
Alex Avila <alex.ramon2004@gmail.com>
2024-07-16 19:23:58 -0500

    feat(test): init tests for account bc.

---
+++ src/test/java/me/ryzeon/bankingsystem/account/AccountCommandServiceTest.java
@@ -0,0 +1,131 @@
+package me.ryzeon.bankingsystem.account;
+
+import me.ryzeon.bankingsystem.account.application.internal.commandservices.AccountCommandServiceImpl;
+import me.ryzeon.bankingsystem.account.application.internal.queryservices.AccountQueryServiceImpl;
+import me.ryzeon.bankingsystem.account.domain.model.aggregates.Account;
+import me.ryzeon.bankingsystem.account.domain.model.commands.CloseAccountCommand;
+import me.ryzeon.bankingsystem.account.domain.model.commands.CreateAccountCommand;
+import me.ryzeon.bankingsystem.account.domain.model.commands.UpdateAccountBalanceCommand;
+import me.ryzeon.bankingsystem.account.domain.model.commands.UpdateAccountDetailsCommand;
+import me.ryzeon.bankingsystem.account.domain.model.exception.AccountAlreadyClosedException;
+import me.ryzeon.bankingsystem.account.domain.model.exception.AccountAlreadyExistsException;
+import me.ryzeon.bankingsystem.account.domain.model.queries.GetAccountHolderByAccountNumberQuery;
+import me.ryzeon.bankingsystem.account.domain.model.valueobjects.AccountInformation;
+import me.ryzeon.bankingsystem.account.infrastructure.persistence.jpa.repositories.AccountRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AccountCommandServiceTest {
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
+    private AccountCommandServiceImpl accountCommandService;
+
+    @InjectMocks
+    private AccountQueryServiceImpl accountQueryService;
+
+    private final Account mockAccount = new Account();
+
+    {
+        mockAccount.setId("123");
+        mockAccount.setBalance(1000.00);
+        mockAccount.setInformation(new AccountInformation("John", "Doe", "john.doe@mail.com", "123456789"));
+        mockAccount.setAccountNumber("1234567890");
+        mockAccount.setActiveAccount(true);
+    }
+
+    @Test
+    void createAccount() {
+        mockAccount.setId(null);
+        when(accountRepository.save(mockAccount)).thenReturn(mockAccount);
+        // Here we simulate the call to the service
+        Account created = accountCommandService.handle(
+                new CreateAccountCommand(
+                        "1234567890",
+                        1000.00,
+                        "John",
+                        "Doe",
+                        "john.doe@mail.com",
+                        "123456789"
+                )
+        ).get();
+
+        assertNotNull(created);
+        assertEquals("1234567890", created.getAccountNumber());
+        assertEquals(1000.00, created.getBalance());
+
+        // Verify
+        verify(accountRepository).save(mockAccount);
+    }
+
+    @Test
+    void createAccountError() {
+        when(accountRepository.findByAccountNumber("1234567890")).thenReturn(Optional.of(mockAccount));
+        assertThrows(AccountAlreadyExistsException.class, () -> accountCommandService.handle(
+                new CreateAccountCommand(
+                        "1234567890",
+                        1000.00,
+                        "John",
+                        "Doe",
+                        "john.doe@mail.com",
+                        "123456789"
+                )));
+    }
+
+    @Test
+    void closeAccount() {
+        when(accountRepository.findByAccountNumber("1234567890")).thenReturn(Optional.of(mockAccount));
+
+        accountCommandService.handle(new CloseAccountCommand("1234567890"));
+
+        assertFalse(mockAccount.isActiveAccount());
+
+        // Verify
+        verify(accountRepository).save(mockAccount);
+    }
+
+    @Test
+    void closeAccountError() {
+        mockAccount.setActiveAccount(false);
+        when(accountRepository.findByAccountNumber("1234567890")).thenReturn(Optional.of(mockAccount));
+        assertThrows(AccountAlreadyClosedException.class, () -> accountCommandService.handle(new CloseAccountCommand("1234567890")));
+    }
+
+    @Test
+    void updateAccountDetails() {
+        when(accountRepository.findByAccountNumber("1234567890")).thenReturn(Optional.of(mockAccount));
+
+        accountCommandService.handle(new UpdateAccountDetailsCommand(
+                "1234567890",
+                "Jane",
+                "Doe",
+                 "jane@mail.com",
+                "987654321"));
+
+        assertEquals("Jane", mockAccount.getInformation().names());
+        assertEquals("Doe", mockAccount.getInformation().lastNames());
+        assertEquals("jane@mail.com", mockAccount.getInformation().email());
+        assertEquals("987654321", mockAccount.getInformation().documentNumber());
+    }
+
+    @Test
+    void updateAccountBalance() {
+        when(accountRepository.findByAccountNumber("1234567890")).thenReturn(Optional.of(mockAccount));
+
+        accountCommandService.handle(new UpdateAccountBalanceCommand("1234567890", 2000.00));
+
+        assertEquals(2000.00, mockAccount.getBalance());
+    }
+}
---
+++ src/test/java/me/ryzeon/bankingsystem/account/AccountQueryServiceTest.java
@@ -0,0 +1,68 @@
+package me.ryzeon.bankingsystem.account;
+
+import me.ryzeon.bankingsystem.account.application.internal.queryservices.AccountQueryServiceImpl;
+import me.ryzeon.bankingsystem.account.domain.model.aggregates.Account;
+import me.ryzeon.bankingsystem.account.domain.model.queries.GetAccountByIdQuery;
+import me.ryzeon.bankingsystem.account.domain.model.queries.GetAccountHolderByAccountNumberQuery;
+import me.ryzeon.bankingsystem.account.domain.model.valueobjects.AccountInformation;
+import me.ryzeon.bankingsystem.account.infrastructure.persistence.jpa.repositories.AccountRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AccountQueryServiceTest {
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
+    private AccountQueryServiceImpl accountQueryService;
+
+    private final Account mockAccount = new Account();
+    {
+        mockAccount.setId("123");
+        mockAccount.setBalance(1000.00);
+        mockAccount.setInformation(new AccountInformation("John", "Doe", "john.doe@mail.com", "123456789"));
+        mockAccount.setAccountNumber("1234567890");
+    }
+
+    @Test
+    void testFindAccountById() {
+        when(accountRepository.findById("123")).thenReturn(Optional.of(mockAccount));
+        // Here we simulate the call to the service
+        Account found = accountQueryService.handle(new GetAccountByIdQuery("123")).get();
+
+        // Assert
+        assertNotNull(found);
+        assertEquals("123", found.getId());
+        assertEquals(1000.00, found.getBalance());
+
+        // Verify
+        verify(accountRepository).findById("123");
+    }
+
+    @Test
+    void testFindByAccountNumber() {
+        when(accountRepository.findByAccountNumber("1234567890")).thenReturn(Optional.of(mockAccount));
+        // Here we simulate the call to the service
+        Account found = accountQueryService.handle(new GetAccountHolderByAccountNumberQuery("1234567890")).get();
+
+        // Assert
+        assertNotNull(found);
+        assertEquals("123", found.getId());
+        assertEquals(1000.00, found.getBalance());
+
+        // Verify
+        verify(accountRepository).findByAccountNumber("1234567890");
+    }
+}
