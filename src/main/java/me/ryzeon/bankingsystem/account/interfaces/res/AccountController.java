1c9d2e68664b4c7d24c50cd15d32fbe0c75166bf
Alex Avila <alex.ramon2004@gmail.com>
2024-07-16 19:23:05 -0500

    feat(account): implemented comments in inbound service.

--- src/main/java/me/ryzeon/bankingsystem/account/interfaces/res/AccountController.java
+++ src/main/java/me/ryzeon/bankingsystem/account/interfaces/res/AccountController.java
@@ -1,11 +1,14 @@
 package me.ryzeon.bankingsystem.account.interfaces.res;
 
 import lombok.AllArgsConstructor;
+import me.ryzeon.bankingsystem.account.domain.model.commands.CloseAccountCommand;
+import me.ryzeon.bankingsystem.account.domain.model.commands.UpdateAccountDetailsCommand;
 import me.ryzeon.bankingsystem.account.domain.model.queries.GetAccountHolderByAccountNumberQuery;
 import me.ryzeon.bankingsystem.account.domain.services.AccountCommandService;
 import me.ryzeon.bankingsystem.account.domain.services.AccountQueryService;
 import me.ryzeon.bankingsystem.account.interfaces.res.resources.AccountResource;
 import me.ryzeon.bankingsystem.account.interfaces.res.resources.CreateAccountResource;
+import me.ryzeon.bankingsystem.account.interfaces.res.resources.UpdateAccountDetailsResource;
 import me.ryzeon.bankingsystem.account.interfaces.res.transforms.AccountResourceFromEntityAssembler;
 import me.ryzeon.bankingsystem.account.interfaces.res.transforms.CreateAccountCommandFromResourceAssembler;
 import org.springframework.http.HttpStatus;
@@ -41,5 +44,18 @@ public class AccountController {
         }
         var accountResource = AccountResourceFromEntityAssembler.toResourceFromEntity(account.get());
         return ResponseEntity.ok(accountResource);
+    }
+
+    @PutMapping("details/{accountNumber}")
+    public ResponseEntity<Void> updateAccountDetails(@PathVariable String accountNumber, @RequestBody UpdateAccountDetailsResource resource) {
+        var command = new UpdateAccountDetailsCommand(accountNumber, resource.names(), resource.lastNames(), resource.email(), resource.documentNumber());
+        accountCommandService.handle(command);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("close/{accountNumber}")
+    public ResponseEntity<Void> closeAccount(@PathVariable String accountNumber) {
+        accountCommandService.handle(new CloseAccountCommand(accountNumber));
+        return ResponseEntity.ok().build();
     }
 }
